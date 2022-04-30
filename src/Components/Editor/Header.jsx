f68bf7e42d9a8c3d2bd19aba22c16a38c2ea2415
kennarddh <kennarddh@gmail.com>
2022-04-30 23:14:25 +0700

    feat: Serialize json and toggle craft.js

--- src/Components/Editor/Header.jsx
+++ src/Components/Editor/Header.jsx
@@ -8,13 +8,28 @@ import {
 	Button as MaterialButton,
 } from '@mui/material'
 
+import { useEditor } from '@craftjs/core'
+
 const Header = () => {
+	const { actions, query, enabled } = useEditor(state => ({
+		enabled: state.options.enabled,
+	}))
+
 	return (
 		<Box px={1} py={1} mt={3} mb={1} bgcolor='#cbe8e7'>
 			<Grid container alignItems='center'>
 				<Grid item xs>
 					<FormControlLabel
-						control={<Switch checked={true} />}
+						control={
+							<Switch
+								checked={enabled}
+								onChange={(_, value) =>
+									actions.setOptions(
+										options => (options.enabled = value)
+									)
+								}
+							/>
+						}
 						label='Enable'
 					/>
 				</Grid>
@@ -23,6 +38,9 @@ const Header = () => {
 						size='small'
 						variant='outlined'
 						color='secondary'
+						onClick={() => {
+							console.log(query.serialize())
+						}}
 					>
 						Serialize JSON to console
 					</MaterialButton>
