c72e7f63aba84e1f27e3c583a0629b976f9ee0ad
kennarddh <kennarddh@gmail.com>
2022-05-01 22:27:17 +0700

    feat: Text setting text

--- src/Components/Editor/Text/Settings.jsx
+++ src/Components/Editor/Text/Settings.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Slider, FormControl, FormLabel } from '@mui/material'
+import { Slider, FormControl, FormLabel, TextField } from '@mui/material'
 
 import { useNode } from '@craftjs/core'
 
@@ -8,25 +8,40 @@ const Settings = () => {
 	const {
 		actions: { setProp },
 		fontSize,
+		text,
 	} = useNode(node => ({
 		fontSize: node.data.props.fontSize,
+		text: node.data.props.text,
 	}))
 
 	return (
-		<FormControl size='small' component='fieldset'>
-			<FormLabel component='legend'>Font size</FormLabel>
-			<Slider
-				color='primary'
-				value={fontSize || 7}
-				step={1}
-				min={7}
-				max={50}
-				onChange={(_, value) => {
-					setProp(props => (props.fontSize = value))
-				}}
-				valueLabelDisplay='auto'
-			/>
-		</FormControl>
+		<>
+			<FormControl size='small' component='fieldset'>
+				<FormLabel component='legend'>Text</FormLabel>
+				<TextField
+					id='standard-basic'
+					variant='standard'
+					value={text}
+					onChange={event =>
+						setProp(props => (props.text = event.target.value))
+					}
+				/>
+			</FormControl>
+			<FormControl size='small' component='fieldset'>
+				<FormLabel component='legend'>Font size</FormLabel>
+				<Slider
+					color='primary'
+					value={fontSize || 7}
+					step={1}
+					min={7}
+					max={50}
+					onChange={(_, value) => {
+						setProp(props => (props.fontSize = value))
+					}}
+					valueLabelDisplay='auto'
+				/>
+			</FormControl>
+		</>
 	)
 }
 
