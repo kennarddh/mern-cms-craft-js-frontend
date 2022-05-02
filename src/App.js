c9ae08c68c15cb71b7625fcecfcba91be1108b91
kennarddh <kennarddh@gmail.com>
2022-05-02 13:49:24 +0700

    refactor: Default node

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Typography, Paper, Grid } from '@mui/material'
 
 import { makeStyles } from '@mui/styles'
 
-import { Editor, Frame, Element } from '@craftjs/core'
+import { Editor, Frame } from '@craftjs/core'
 
 import Toolbox from 'Components/Editor/Toolbox'
 import SettingsPanel from 'Components/Editor/SettingsPanel'
@@ -14,6 +14,7 @@ import Container from 'Components/Editor/Container/Container'
 import Button from 'Components/Editor/Button/Button'
 import Card, { CardTop, CardBottom } from 'Components/Editor/Card/Card'
 import Text from 'Components/Editor/Text/Text'
+import DefaultNode from 'Default/DefaultNode'
 
 const useStyles = makeStyles(() => ({
 	root: {
@@ -47,40 +48,7 @@ export default function App() {
 				<Topbar />
 				<Grid container spacing={5} style={{ paddingTop: '10px' }}>
 					<Grid item xs>
-						<Frame>
-							<Element
-								canvas
-								is={Container}
-								padding={5}
-								background='#eeeeee'
-								data-cy='root-container'
-							>
-								<Card data-cy='frame-card' />
-								<Button
-									size='small'
-									variant='contained'
-									text='Click me'
-								/>
-								<Text
-									fontSize={20}
-									text='Hi world!'
-									data-cy='frame-text'
-								/>
-								<Element
-									canvas
-									is={Container}
-									padding={6}
-									background='#999999'
-									data-cy='frame-container'
-								>
-									<Text
-										size='small'
-										text="It's me again!"
-										data-cy='frame-container-text'
-									/>
-								</Element>
-							</Element>
-						</Frame>
+						<Frame data={DefaultNode} />
 					</Grid>
 					<Grid item xs={4}>
 						<Paper className={classes.root}>
