e13717afa8ffc2da7566ecc0d3325f9b99adafd1
kennarddh <kennarddh@gmail.com>
2022-05-05 22:29:05 +0700

    feat: Container margin settings

--- src/Components/Editor/Nodes/Container/Container.jsx
+++ src/Components/Editor/Nodes/Container/Container.jsx
@@ -6,7 +6,15 @@ import { Paper } from '@mui/material'
 
 import Settings from './Settings'
 
-const Container = ({ background, padding = 0, children }) => {
+const Container = ({
+	background,
+	padding = 0,
+	marginTop,
+	marginRight,
+	marginBottom,
+	marginLeft,
+	children,
+}) => {
 	const {
 		connectors: { connect, drag },
 	} = useNode(state => {
@@ -18,7 +26,14 @@ const Container = ({ background, padding = 0, children }) => {
 	return (
 		<Paper
 			ref={ref => connect(drag(ref))}
-			style={{ margin: '5px 0', background, padding: `${padding}px` }}
+			style={{
+				marginTop: `${marginTop}px`,
+				marginRight: `${marginRight}px`,
+				marginBottom: `${marginBottom}px`,
+				marginLeft: `${marginLeft}px`,
+				background,
+				padding: `${padding}px`,
+			}}
 		>
 			{children}
 		</Paper>
@@ -28,6 +43,10 @@ const Container = ({ background, padding = 0, children }) => {
 Container.craft = {
 	props: {
 		padding: 20,
+		marginTop: 5,
+		marginRight: 0,
+		marginBottom: 5,
+		marginLeft: 0,
 	},
 	related: {
 		settings: Settings,
--- src/Components/Editor/Nodes/Container/Settings.jsx
+++ src/Components/Editor/Nodes/Container/Settings.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { useNode } from '@craftjs/core'
 
@@ -6,22 +6,105 @@ import { FormControl, FormLabel, Slider } from '@mui/material'
 
 const Settings = () => {
 	const {
+		id,
 		padding,
+		marginTop,
+		marginRight,
+		marginBottom,
+		marginLeft,
 		actions: { setProp },
 	} = useNode(node => ({
 		padding: node.data.props.padding,
+		marginTop: node.data.props.marginTop,
+		marginRight: node.data.props.marginRight,
+		marginBottom: node.data.props.marginBottom,
+		marginLeft: node.data.props.marginLeft,
 	}))
 
+	const [PaddingValue, SetPaddingValue] = useState(padding)
+	const [MarginTopValue, SetMarginTopValue] = useState(marginTop)
+	const [MarginRightValue, SetMarginRightValue] = useState(marginRight)
+	const [MarginBottomValue, SetMarginBottomValue] = useState(marginBottom)
+	const [MarginLeftValue, SetMarginLeftValue] = useState(marginLeft)
+
+	useEffect(() => {
+		setProp(props => (props.padding = PaddingValue), 500)
+	}, [PaddingValue])
+
+	useEffect(() => {
+		setProp(props => (props.marginTop = MarginTopValue), 500)
+	}, [MarginTopValue])
+
+	useEffect(() => {
+		setProp(props => (props.marginRight = MarginRightValue), 500)
+	}, [MarginRightValue])
+
+	useEffect(() => {
+		setProp(props => (props.marginBottom = MarginBottomValue), 500)
+	}, [MarginBottomValue])
+
+	useEffect(() => {
+		setProp(props => (props.marginLeft = MarginLeftValue), 500)
+	}, [MarginLeftValue])
+
 	return (
-		<FormControl fullWidth={true} margin='normal' component='fieldset'>
-			<FormLabel component='legend'>Padding</FormLabel>
-			<Slider
-				defaultValue={padding}
-				onChange={(_, value) =>
-					setProp(props => (props.padding = value), 500)
-				}
-			/>
-		</FormControl>
+		<>
+			<FormControl fullWidth={true} margin='normal' component='fieldset'>
+				<FormLabel component='legend'>Padding</FormLabel>
+				<Slider
+					value={PaddingValue}
+					onChange={(_, value) => SetPaddingValue(value)}
+				/>
+			</FormControl>
+			{id !== 'ROOT' ? (
+				<>
+					<FormControl
+						fullWidth={true}
+						margin='normal'
+						component='fieldset'
+					>
+						<FormLabel component='legend'>Margin Top</FormLabel>
+						<Slider
+							value={MarginTopValue}
+							onChange={(_, value) => SetMarginTopValue(value)}
+						/>
+					</FormControl>
+					<FormControl
+						fullWidth={true}
+						margin='normal'
+						component='fieldset'
+					>
+						<FormLabel component='legend'>Margin Right</FormLabel>
+						<Slider
+							value={MarginRightValue}
+							onChange={(_, value) => SetMarginRightValue(value)}
+						/>
+					</FormControl>
+					<FormControl
+						fullWidth={true}
+						margin='normal'
+						component='fieldset'
+					>
+						<FormLabel component='legend'>Margin Bottom</FormLabel>
+						<Slider
+							value={MarginBottomValue}
+							onChange={(_, value) => SetMarginBottomValue(value)}
+						/>
+					</FormControl>
+					<FormControl
+						fullWidth={true}
+						margin='normal'
+						component='fieldset'
+					>
+						<FormLabel component='legend'>Margin Left</FormLabel>
+						<Slider
+							value={MarginLeftValue}
+							onChange={(_, value) => SetMarginLeftValue(value)}
+						/>
+					</FormControl>
+				</>
+			) : null}
+		</>
 	)
 }
 
