c1dd2cd5e22357f16e97b7f47b44af99893c71da
kennarddh <kennarddh@gmail.com>
2022-05-07 22:53:59 +0700

    feat: Padding card

--- src/Components/Editor/Nodes/Card/Card.jsx
+++ src/Components/Editor/Nodes/Card/Card.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
-import Text from 'Components/Editor/Nodes/Text/Text'
-import Button from 'Components/Editor/Nodes/Button/Button'
+
 import { Element } from '@craftjs/core'
 
-import Container from 'Components/Editor/Nodes/Container/InnerContainer'
+import Text from 'Components/Editor/Nodes/Text/Text'
+import Button from 'Components/Editor/Nodes/Button/Button'
+
+import InnerContainer from 'Components/Editor/Nodes/Container/InnerContainer'
 
 import CardTop from './CardTop'
 import CardBottom from './CardBottom'
 
-const Card = ({ background, padding = 20 }) => {
+const Card = ({
+	background,
+	paddingTop = 20,
+	paddingRight = 20,
+	paddingBottom = 20,
+	paddingLeft = 20,
+}) => {
 	return (
-		<Container background={background} padding={padding}>
+		<InnerContainer
+			background={background}
+			paddingTop={paddingTop}
+			paddingRight={paddingRight}
+			paddingBottom={paddingBottom}
+			paddingLeft={paddingLeft}
+		>
 			<Element id='text' is={CardTop} canvas>
 				<Text text='Title' fontSize={20} />
 				<Text text='Subtitle' fontSize={15} />
@@ -18,7 +32,7 @@ const Card = ({ background, padding = 20 }) => {
 			<Element id='buttons' is={CardBottom} canvas>
 				<Button size='small' text='Learn more' variant='contained' />
 			</Element>
-		</Container>
+		</InnerContainer>
 	)
 }
 
